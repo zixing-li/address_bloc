d67ed16cf99392305cfa57feea8c01c2cee187eb
Luke Li <zl82@cornell.edu>
2017-12-14 03:05:46 +0000

    checkpoint 6 assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -39,4 +39,4 @@ class AddressBook
     end
   end
   
- end
+end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -99,5 +99,34 @@ RSpec.describe AddressBook do
      end
    end
    
-   
- end
+     describe "#importing entries_2" do
+       it "imports from the 2nd set of entries" do
+         book.import_from_csv("entries_2.csv")
+         expect(book.entries.size).to eq(3)
+       
+       book.import_from_csv("entries_2.csv")
+       # Check the first entry
+       entry_one = book.entries[0]
+ 
+       check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+     end
+
+     it "imports the 2nd entry" do
+       book.import_from_csv("entries_2.csv")
+       # Check the second entry
+       entry_two = book.entries[1]
+
+       check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+     end
+
+     it "imports the 3rd entry" do
+       book.import_from_csv("entries_2.csv")
+       # Check the third entry
+       entry_three = book.entries[2]
+
+       check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+     end
+    end
+         
+       
+end
