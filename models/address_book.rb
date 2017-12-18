60c0729bb4b54acb98f18027bf6529503b598749
Luke Li <zl82@cornell.edu>
2017-12-18 00:16:54 +0000

    checkpoint 7 assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -64,5 +64,14 @@ class AddressBook
      # #5
      return nil
    end
-   
+  
+  def iterative_search(name)
+    @entries.each do |entry|
+    if name == entry.name
+      return entry
+    end
+    end
+    return nil
+  end
+  
 end
